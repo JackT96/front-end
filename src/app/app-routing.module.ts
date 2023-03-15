0de29a85eae48f1bff820d1b9ccba3e5ac69d3f4
Jack Tiew <117332708+JackT96@users.noreply.github.com>
2023-03-15 15:16:05 +0800

    Added project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'student/student-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'student',
+    loadChildren: () => import('./components/student/student.module').then((m) => m.StudentModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
